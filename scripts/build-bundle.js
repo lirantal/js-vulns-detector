660ee06dd9587870dc300f4ccfdbd0bcaa628ba0
Liran Tal <liran.tal@gmail.com>
2020-04-06 16:06:40 +0300

    refactor: use rollup api

--- scripts/build-bundle.js
+++ scripts/build-bundle.js
@@ -1,12 +1,45 @@
 /* eslint-disable security/detect-non-literal-fs-filename */
+
 const fs = require('fs')
 const path = require('path')
-const sourceBundleFilepath = '../dist/bundle-base.js'
-const targetBundleFilepath = '../dist/bundle-global.js'
-const bundle = fs.readFileSync(path.join(__dirname, sourceBundleFilepath))
+const rollup = require('rollup')
+const resolve = require('@rollup/plugin-node-resolve')
+const commonjs = require('@rollup/plugin-commonjs')
+const json = require('@rollup/plugin-json')
 
-const outputString = `${bundle};
+const targetBundleFilepath = path.join(__dirname, '../', 'dist', 'bundle-global.js')
 
-return YWxseW91cmJhc2VhcmViZWxvbmd0b3Vz();`
+async function transpileJSBundle({ inputOptions, outputOptions }) {
+  const bundle = await rollup.rollup(inputOptions)
+  const { output } = await bundle.generate(outputOptions)
+  return output
+}
 
-fs.writeFileSync(path.join(__dirname, targetBundleFilepath), outputString)
+async function createJSBundle() {
+  const inputOptions = {
+    input: path.join(__dirname, '../', 'src/main.js'),
+    treeshake: false,
+    plugins: [json(), resolve(), commonjs()]
+  }
+
+  const outputOptions = {
+    output: {
+      file: 'dist/bundle-base.js',
+      format: 'esm'
+    }
+  }
+
+  const bundleData = await transpileJSBundle({ inputOptions, outputOptions })
+  let codeAsString = ''
+
+  for (const chunk of bundleData) {
+    if (chunk.type === 'chunk' && chunk.code) {
+      codeAsString += chunk.code
+    }
+  }
+
+  codeAsString += ';\nreturn YWxseW91cmJhc2VhcmViZWxvbmd0b3Vz();'
+  fs.writeFileSync(targetBundleFilepath, codeAsString)
+}
+
+createJSBundle()
